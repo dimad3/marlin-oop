a9a8b2e3d66c07b0390a8db4068b2e1f40f6d6db
dimad3 <dmitrijs633@gmail.com>
2020-06-05 19:32:02 +0300

    Lecture 9. Config class

--- classes/Database.php
+++ classes/Database.php
@@ -9,9 +9,12 @@ private $pdo, $query, $error = false, $results, $count;
 private function __construct()
 {
     try {
-        $this->pdo = new PDO('mysql:host=localhost; dbname=test; charset=utf8',
-        'root',
-        '');
+        // $this->pdo = new PDO('mysql:host=localhost; dbname=test; charset=utf8', 'root', '');
+        $this->pdo = new PDO(
+            "mysql:host=" . Config::get('mysql.host') . "; dbname=" . Config::get('mysql.database'),
+            Config::get('mysql.username'),
+            Config::get('mysql.password')
+        );
         // echo 'ok';
     } catch (PDOException $exception) {
         die($exception->getMessage());
--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-require_once 'Database.php';
+require_once 'classes/Database.php';
+require_once 'classes/Config.php';
+
+// var_dump($GLOBALS); die;
+// add our own element in the $GLOBALS array
+$GLOBALS['config'] = [ // in init.php
+    'mysql' => [
+        'host'      => 'localhost',
+        'username'  => 'root',
+        'password'  => '',
+        'database'  => 'test',
+        'something' => [
+            'no'    => [
+                'foo'   => [
+                    'bar'   => 'baz'
+                ]
+            ]
+        ]
+    ]
+
+];
+echo Config::get('mysql.something.no.foo.bar'); // baz
 
 // $users = Database::getinstance()->query("SELECT * FROM `users` WHERE username = IN (?,?)", ['John Doe', 'Jane Koe']);
 
@@ -29,5 +50,7 @@ require_once 'Database.php';
 //     ]);
 // var_dump($updateduser);
 
-$users = Database::getinstance()->get('users', ['password', '=', '1234']);
-var_dump($users->first());
+// $users = Database::getinstance()->get('users', ['password', '=', '1234']);
+// var_dump($users->first());
+
+
